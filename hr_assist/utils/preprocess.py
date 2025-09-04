376d7faa32aaa548c218083d7597f011578e487f
Miklos Hamar <hamiki987@gmail.com>
2025-09-04 14:35:52 +0200

    doc to md utility

--- hr_assist/utils/preprocess.py
+++ hr_assist/utils/preprocess.py
@@ -2,3 +2,18 @@
 Utility module for preprocessing user-supplied inputs.
 """
 
+from pathlib import Path
+import pymupdf4llm
+
+
+def doc_to_md(input_bin: bytes):
+    page_chunked = pymupdf4llm.to_markdown(input_bin, page_chunks=True)
+    converted_doc = "\n\n".join([chunk['text'] for chunk in page_chunked])
+    metadata = page_chunked[0].get('metadata', {}) if page_chunked else {}
+    metadata = metadata.copy(); metadata.pop('page', None)
+
+    return converted_doc, page_chunked, metadata
+
+def doc_file_to_md(file_path: Path):
+    with open(file_path, "rb") as f:
+        return doc_to_md(f.read())
