9831633570800f11752bc74fb9ab44517eb39e25
Miklos Hamar <hamiki987@gmail.com>
2025-09-20 17:51:46 +0200

    wip search pipeline

--- hr_assist/search/pipeline.py
+++ hr_assist/search/pipeline.py
@@ -1,8 +1,23 @@
 
 
+from typing import Optional
+from hr_assist.search.ranking import BaseRanker, BaseReranker
+from ..db import BaseDb
+
+
 class HRSearchPipeline:
     def __init__(
         self,
-        retriever,
-        ranker,
-        )
+        db: BaseDb,
+        ranker: BaseRanker,
+        reranker: Optional[BaseReranker] = None,
+        # hyper parameters
+        rank_k: int = 500,
+        ):
+        self.ranker = ranker
+        self.reranker = reranker
+        self.db = db
+    
+    def search(self, job_description: str) -> list:
+        # step 1: retrieve
+        results = self.ranker.rank(query=job_description, )
