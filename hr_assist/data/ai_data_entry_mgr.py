ea13ee661b96ae9ead31ee5e4af96b897d77940d
Miklos Hamar <hamiki987@gmail.com>
2025-10-13 12:33:36 +0200

    add ai data entry mgr dataset placeholder

---
+++ hr_assist/data/ai_data_entry_mgr.py
@@ -0,0 +1,8 @@
+"""Dataset-specific definition of the RawDataHandler for the AI Data Entry Mgr dataset."""
+
+from .raw_data import RawDataHandler
+
+class AIDataEntryMgrHandler(RawDataHandler):
+    """Implementation omitted."""
+    pass
+
